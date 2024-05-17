42fba5020f47ba5ac1cc2ecdb1074a5084e15873
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-05-18 02:15:58 +0300

    Refactor layout components and update providers import

--- app/(nav-pages)/layout.js
+++ app/(nav-pages)/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
-import { Providers } from "../app/providers";
+import { Providers } from "../providers.jsx";
 import Nav from "../components/nav-bar/Nav-bar";
 
 const inter = Inter({ subsets: ["latin"] });
