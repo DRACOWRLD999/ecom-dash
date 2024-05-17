d2736023d010bae2d0dff20507d8bae761acd147
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-05-17 15:19:19 +0300

    finished navbar

---
+++ app/components/nav-bar/Nav-bar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Button,Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+import Logo from '../logo/Logo';
+
+
+export default function Nav() {
+  return (
+    <div>
+        <Navbar className='flex flex-row' isBordered='true'>
+            <NavbarBrand className=' flex '>
+                <Logo/>
+                <p className="font-bold text-inherit p-4">DASH</p>
+            </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+              <Link href="" className=' text-secondary-foreground hover:text-primary-800'>
+                Home
+              </Link>
+          </NavbarItem>
+        <NavbarItem>
+          <Link href="cart"  className=' text-secondary-foreground hover:text-primary-800'>
+            Carts
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="user" className=' text-secondary-foreground hover:text-primary-800'>
+            Users
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="sign" variant="flat">
+            Sign In
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+        </Navbar>
+    </div>
+  )
+}
