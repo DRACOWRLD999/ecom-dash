6e2fbdaa9d275dffbd025432c6cf47837ff8311d
DRACOWRLD999 <hmoodhmood004@gmail.com>
2024-05-19 23:29:48 +0300

    removed sign in

--- app/(non-nav)/sign/page.jsx
+++ app/(non-nav)/sign/page.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex flex-col justify-center'>
-      <h1 className='text-4xl text-center'>Sign In</h1>
-      <form className='flex flex-col justify-center'>
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' />
-        <label htmlFor='password'>Password</label>
-        <input type='password' id='password' />
-        <Button>Sign In</Button>
-      </form>
+    <div className='grid w-96 place-items-center'>
     </div>
   )
 }
